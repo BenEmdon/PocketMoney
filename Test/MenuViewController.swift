befac07bad4d37eafab0f006de4457d52733c83b
BenEmdon <benjaminemdon@gmail.com>
2015-12-28 08:58:22 -0500

    Initial push

---
+++ Test/MenuViewController.swift
@@ -0,0 +1,19 @@
+//
+//  MenuViewController.swift
+//  Test
+//
+//  Created by Benjamin Emdon on 2015-12-27.
+//  Copyright © 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+class MenuViewController: UIViewController {
+
+    
+    @IBAction func exitButtonDidPush(sender: AnyObject) {
+        dismissViewControllerAnimated(true, completion: nil)
+    }
+    
+
+}
---
+++ Test/StoryTableViewCell.swift
@@ -0,0 +1,17 @@
+//
+//  StoryTableViewCell.swift
+//  Test
+//
+//  Created by Benjamin Emdon on 2015-12-27.
+//  Copyright © 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+class StoryTableViewCell: UITableViewCell {
+
+    @IBOutlet var badgeImage: UIImageView!
+    @IBOutlet var amountTitle: UILabel!
+
+
+}
