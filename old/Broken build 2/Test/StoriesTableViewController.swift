96d33b66917d479af8ecc5a1e12887f9601eb0c6
BenEmdon <benjaminemdon@gmail.com>
2016-01-04 23:19:59 -0500

    Added data model and broke build

---
+++ old/Broken build 2/Test/StoriesTableViewController.swift
@@ -0,0 +1,125 @@
+//
+//  StoriesTableViewController.swift
+//  Test
+//
+//  Created by Benjamin Emdon on 2015-12-27.
+//  Copyright © 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+class StoriesTableViewController: UITableViewController, StoryTableViewCellDelegate {
+    
+    //var dataList:[Values] = testData
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        /*UIApplication.sharedApplication().setStatusBarStyle(UIStatusBarStyle.LightContent ,animated: true)    //Depreciated  */
+        
+        tableView.estimatedRowHeight = 46
+        
+        
+    }
+    
+    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("InfoSegue", sender: self)
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    @IBAction func menuButtonDidPress(sender: AnyObject) {
+        performSegueWithIdentifier("MenuSegue", sender: self)
+    }
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 1//dataList.count
+    }
+    
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        let cell = tableView.dequeueReusableCellWithIdentifier("StoryCell")! as! StoryTableViewCell
+        
+        //let CellData = dataList[indexPath.row] as Values
+        
+        cell.amountLabel.text = "$14.40"//CellData.amountString
+        cell.timeStampLabel.text = "8h"
+        cell.badgeImage.image = UIImage(named: "Minus")
+        
+        cell.delegate = self
+        
+        return cell
+    }
+    
+    override func tableView(tableView: UITableView, didDeselectRowAtIndexPath indexPath: NSIndexPath) {
+        performSegueWithIdentifier("InfoSegue", sender: self)
+        
+        tableView.deselectRowAtIndexPath(indexPath, animated: true)
+    }
+    
+    
+    //MARK: Not mine
+    /*
+    func timeAgoSinceDate(date:NSDate, numericDates:Bool) -> String {
+        let calendar = NSCalendar.currentCalendar()
+        let now = NSDate()
+        let earliest = now.earlierDate(date)
+        let latest = (earliest == now) ? date : now
+        let components:NSDateComponents = calendar.components([NSCalendarUnit.Minute , NSCalendarUnit.Hour , NSCalendarUnit.Day , NSCalendarUnit.WeekOfYear , NSCalendarUnit.Month , NSCalendarUnit.Year , NSCalendarUnit.Second], fromDate: earliest, toDate: latest, options: NSCalendarOptions())
+        
+        if (components.year >= 2) {
+            return "\(components.year) years ago"
+        } else if (components.year >= 1){
+            if (numericDates){
+                return "1 year ago"
+            } else {
+                return "Last year"
+            }
+        } else if (components.month >= 2) {
+            return "\(components.month) months ago"
+        } else if (components.month >= 1){
+            if (numericDates){
+                return "1 month ago"
+            } else {
+                return "Last month"
+            }
+        } else if (components.weekOfYear >= 2) {
+            return "\(components.weekOfYear) weeks ago"
+        } else if (components.weekOfYear >= 1){
+            if (numericDates){
+                return "1 week ago"
+            } else {
+                return "Last week"
+            }
+        } else if (components.day >= 2) {
+            return "\(components.day) days ago"
+        } else if (components.day >= 1){
+            if (numericDates){
+                return "1 day ago"
+            } else {
+                return "Yesterday"
+            }
+        } else if (components.hour >= 2) {
+            return "\(components.hour) hours ago"
+        } else if (components.hour >= 1){
+            if (numericDates){
+                return "1 hour ago"
+            } else {
+                return "An hour ago"
+            }
+        } else if (components.minute >= 2) {
+            return "\(components.minute) minutes ago"
+        } else if (components.minute >= 1){
+            if (numericDates){
+                return "1 minute ago"
+            } else {
+                return "A minute ago"
+            }
+        } else if (components.second >= 3) {
+            return "\(components.second) seconds ago"
+        } else {
+            return "Just now"
+        }
+        
+    }*/
+}
---
+++ old/Broken build 2/Test/StoryTableViewCell.swift
@@ -0,0 +1,20 @@
+//
+//  StoryTableViewCell.swift
+//  Test
+//
+//  Created by Benjamin Emdon on 2015-12-27.
+//  Copyright © 2015 Ben Emdon. All rights reserved.
+//
+
+import UIKit
+
+protocol StoryTableViewCellDelegate: class {
+}
+
+class StoryTableViewCell: UITableViewCell {
+    
+    @IBOutlet weak var badgeImage: UIImageView!
+    @IBOutlet weak var amountLabel: UILabel!
+    @IBOutlet weak var timeStampLabel: UILabel!
+    weak var delegate: StoryTableViewCellDelegate?
+}
